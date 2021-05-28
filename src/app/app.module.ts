ef9a905f9b1e1e7366178ee2339c10347afe3c34
gauravthapaliya261 <gauravthapaliya261@gmail.com>
2021-05-28 17:37:50 +0545

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TagInputModule } from 'ngx-chips';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    TagInputModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,139 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  inputTypes = [
+    {
+      label: 'Text',
+      name: 'text'
+    },
+    {
+      label: 'Textarea',
+      name: 'textarea'
+    },
+    {
+      label: 'Checkbox',
+      name: 'checkbox'
+    },
+    {
+      label: 'Radio Button',
+      name: 'radio'
+    },
+    {
+      label: 'Select Dropdown',
+      name: 'select'
+    },
+    {
+      label: 'Toggle Switch',
+      name: 'toggle'
+    },
+    {
+      label: 'Date',
+      name: 'date'
+    },
+    {
+      label: 'Time',
+      name: 'time'
+    },
+    {
+      label: 'DateTime',
+      name: 'datetime'
+    },
+  ];
+
+  listItems = [];
+  outputArray = [];
+  formBasic: FormGroup;
+  addNewForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.buildAddNewForm();
+  }
+
+  buildForm(): void {
+    this.formBasic = this.fb.group({}, { updateOn: 'change' });
+    for (const i of this.listItems) {
+      this.formBasic.addControl(i.label, this.fb.control(''));
+    }
+  }
+
+  buildAddNewForm(): void {
+    this.addNewForm = this.fb.group({
+      type: ['', Validators.required],
+      label: ['', Validators.required],
+      placeholder: [''],
+      options: [''],
+      isRequired: [true]
+    });
+  }
+
+  get type() {
+    return this.addNewForm.get('type');
+  }
+  get label() {
+    return this.addNewForm.get('label');
+  }
+  get options() {
+    return this.addNewForm.get('options');
+  }
+
+  addNew(): void {
+    if (this.formBasic.contains(this.addNewForm.value.label)) {
+      this.toastr.warning('Input field with label ' + this.addNewForm.value.label + ' already added.', 'Oops !', { timeOut: 3000 });
+    } else {
+      this.listItems.push(this.addNewForm.value);
+      this.formBasic.addControl(this.addNewForm.value.label,
+        this.fb.control('', this.addNewForm.value.isRequired ? Validators.required : null));
+    }
+    this.addNewForm.reset();
+    this.addNewForm.controls.isRequired.setValue(true);
+    this.addNewForm.controls.type.setValue('');
+  }
+
+  changeType(event): void {
+    if (event.target.value === 'radio' || event.target.value === 'select') {
+      this.options.setValidators([Validators.required]);
+    } else {
+      this.options.clearValidators();
+    }
+    this.options.updateValueAndValidity();
+  }
+
+  deleteField(item): void {
+    const index = this.listItems.indexOf(item);
+    this.listItems.splice(index, 1);
+    this.formBasic.removeControl(item.label);
+    this.toastr.info('Item deleted successfully', 'Success !', { timeOut: 3000 });
+    this.submit();
+  }
+
+  clearAll(): void {
+    this.listItems = [];
+    this.formBasic = this.fb.group([]);
+    this.submit();
+    this.toastr.success('All items cleared.', 'Success !', { timeOut: 3000 });
+  }
+  submit(): void {
+    this.outputArray = [];
+    // tslint:disable-next-line: forin
+    for (const element in this.formBasic.value) {
+      this.outputArray.push({
+        name: element,
+        value: this.formBasic.value[element]
+      });
+    }
+  }
+}
